4925f14a3c02d91ef11089f763f3ff230a45e8f2
Miguel <josemigueltrujillot@gmail.com>
2020-12-01 16:17:27 -0500

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div id="general">
+    <header>
+        <div id="cabecera">
+        <img id="m" src="img/logo-ferreteria.png">
+        <img id="im" src="img/admin.png">
+        <img id="img" src="img/carrito.png">
+        
+        <nav>
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">CATEGORIA DE PRODUCTOS</a></li>
+                <li><a href="">CONTACTANOS</a></li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+    
+    <div id="logo">
+    <img id="logo" src="img/log.jpg">
+    </div>
+    
+    <div id="titulo">
+    <h3 id="til">CATEGORIA DE PRODUCTOS</h3>
+    </div>
+    
+    <div id="iconos">
+    <img id="icon" src="img/ferreteria.png">
+    <img id="icon" src="img/construccion.png">
+    <img id="icon" src="img/electricos.png">
+    <img id="icon" src="img/pinturas.png">
+    <img id="icon" src="img/herramientas.png">
+    <img id="icon" src="img/soldadura.png">
+    </div>
+    </div>
+</body>
+</html>
